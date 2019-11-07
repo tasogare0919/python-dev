97acefc210b43a56d990386aee176bb22c1f6c84
Sadayoshi Tada <tadainfra@gmail.com>
2019-11-07 18:49:00 +0900

    ランダム処理の改修

--- aws-lambda-dev/redmine-checker-post.py
+++ aws-lambda-dev/redmine-checker-post.py
@@ -1,23 +1,26 @@
 # -*- coding:utf-8 -*-
-import random
 import os
+import random
 import json
 import urllib.request
 
+
 def lambda_handler(event, context):
     post_slack()
-    return "End"
+    return ("End")
 
 def post_slack():
-    user_list = ["USERID"]
-    counter = random.randint(0, 2)
+    user_list = ["U0KTQF99R", "UGSEMJ9EK", "UHD6KN35F"]
+    counter = [0, 1, 2]
+    random.shuffle(counter)
     method = "POST"
+    print("Today's Redmine Checker Pick up Start.")
 
     send_data = {
-        'username': 'UserName',
-        'channel': '#Channel',
-        'icon_emoji': 'emojiicon',
-        'text': '今日の当番は<@' + user_list[counter] + '>です',
+        'username': 'Redmine-Checker',
+        'channel': '#channel',
+        'icon_emoji': ':iconalias:',
+        'text': '10時の当番は<@' + user_list[counter[0]] + '>です\r\n```URLS```'
     }
 
     send_text = ("payload=" + json.dumps(send_data)).encode('utf-8')
